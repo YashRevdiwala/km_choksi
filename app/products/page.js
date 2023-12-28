c3b518d89aa803b45cb653c69f43113a08e27bf7
Yash Revdiwala <yash.yr.2493@gmail.com>
2023-12-28 17:56:40 +0530

    final commit

--- app/products/page.js
+++ app/products/page.js
@@ -1,12 +1,3 @@
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination"
 import Footer from "../(components)/Footer"
 import Header from "../(components)/Header"
 import { ProductPagination } from "../(components)/Pagintion"
